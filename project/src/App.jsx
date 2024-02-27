c4749506a2ecd0df5cf14d61cce33aaaaff650a5
Xander Vanparys <xander.vanparys@ugent.be>
2024-02-27 14:55:04 +0100

    fix errors

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import Room from './components/Room'
 import './App.css'
 
 function App() {
