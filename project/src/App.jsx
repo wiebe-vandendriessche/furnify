06f7a1c67f399f7580abae31269ff47eea9bce2d
Wiebe Vandendriessche <wiebe.vandendriessche@ugent.be>
2024-02-28 14:47:28 +0100

    adding sky and sun

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Sky, Stars } from '@react-three/drei'
 import Room from './components/Room'
+
+import * as THREE from 'three';
 import './App.css'
 
 function App() {
@@ -55,15 +57,20 @@ function App() {
           <button type="submit">Submit</button>
         </div>
       </form>
-      <Canvas className="canvas">
-        {/* <Room width={width} height={height} depth={depth} /> */}
-        <ambientLight intensity={0.3}></ambientLight>
-        <directionalLight position={[0, 15, 15]}/>
+      <Canvas className="canvas" >
         <Room width={width} depth={depth} height={height} wallThickness={0.3} floorThickness={0.3} />
+        <ambientLight intensity={.5}/>
+        <directionalLight position={[0, 100, 100]}/>
+        
         <OrbitControls />
+
+        <Sky distance={450000} sunPosition={[0, 100, 100]} inclination={10} azimuth={0.25} />
+        
+        <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
+
         <axesHelper />
       </Canvas>
-    </div>  
+    </div>
   );
 }
 
