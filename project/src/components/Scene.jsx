7223ef5ef88a329083963efc9d967f0c4559bf07
Wiebe Vandendriessche <wiebe.vandendriessche@ugent.be>
2024-03-03 21:59:14 +0100

    fix scenary, added ground, better sky and fog

--- project/src/components/Scene.jsx
+++ project/src/components/Scene.jsx
@@ -4,21 +4,25 @@ import { Canvas } from '@react-three/fiber';
 import { useState } from 'react'
 import { OrbitControls, Sky, Stars } from '@react-three/drei'
 import { Room } from './roomComponents/Room.tsx'
+import { Ground } from './other/Ground.jsx'
+
+
 
 const Scene = () => {
     const { width, height, depth } = useDimensions();
     return (
-        <Canvas className="canvas" camera={{position: [10, 6, 8]}}>
+        <Canvas className="canvas" camera={{position: [10, 6, 8]}} style={{ backgroundColor: 'lightblue' }}>
             <ambientLight intensity={.5} />
             <directionalLight position={[0, 100, 100]} />
-            <Room width={width} depth={depth} height={height} wallThickness={0.3} floorThickness={0.3} />
-
+            <Room width={width} depth={depth} height={height} wallThickness={0.3} floorThickness={0.3}/>
+            <fog attach="fog" args={['lightblue', 1, 500]} />
+            <Ground />
             <OrbitControls />
 
-            <Sky distance={450000} sunPosition={[0, 100, 100]} inclination={10} azimuth={0.25} />
 
-            <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
-
+            <Stars radius={500} depth={50} count={8000} factor={15} saturation={50} fade speed={1} />
+            
+            <hemisphereLight color="lightblue" groundColor="0xf7e497" intensity={0.5}/>
             <axesHelper />
         </Canvas>
     )
