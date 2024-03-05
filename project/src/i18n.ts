f4c1cfd27d1273ef73f3c27d5dbd57de8ab38bce
Wiebe Vandendriessche <wiebe.vandendriessche@ugent.be>
2024-03-05 10:51:26 +0100

    Adding some translations to begin

--- project/src/i18n.ts
+++ project/src/i18n.ts
@@ -3,42 +3,66 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-  .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    debug: true,
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
-    },
-    resources: {
-      en: {
-        translation: {
-            greeting: {
-                hello: "Hello World!"
+    // detect user language
+    // learn more: https://github.com/i18next/i18next-browser-languageDetector
+    .use(LanguageDetector)
+    // pass the i18n instance to react-i18next.
+    .use(initReactI18next)
+    // init i18next
+    // for all options read: https://www.i18next.com/overview/configuration-options
+    .init({
+        debug: true,
+        fallbackLng: 'en',
+        interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+        },
+        resources: {
+            en: {
+                translation: {
+                    greeting: {
+                        hello: "Hello World!"
+                    },
+                    questionnaire_space: {
+                        rectangular: "Rectangular",
+                        length: "Length",
+                        width: "Width",
+                        height: "Height",
+                        other: "Other",
+                        add_aspect: "Add aspect"
+                    }
+                }
+            },
+            nl: {
+                translation: {
+                    greeting: {
+                        hello: "Hallo Wereld!"
+                    },
+                    questionnaire_space: {
+                        rectangular: "Rechthoekig",
+                        length: "Lengte",
+                        width: "Breedte",
+                        height: "Hoogte",
+                        other: "Anders",
+                        add_aspect: "Voeg een aspect toe"
+                    }
+                }
+            },
+            fr: {
+                translation: {
+                    greeting: {
+                        hello: "Bonjour le monde!"
+                    },
+                    questionnaire_space: {
+                        rectangular: "Rectangulaire",
+                        length: "Longueur",
+                        width: "Largeur",
+                        height: "Hauteur",
+                        other: "Autres",
+                        add_aspect: "Ajouter un aspect"
+                    }
+                }
             }
         }
-      },
-      nl: {
-        translation: {
-            greeting: {
-                hello: "Hallo Wereld!"
-            }
-        }
-      },
-      fr: {
-        translation: {
-            greeting: {
-                hello: "Bonjour le monde!"
-            }
-        }
-      }
-    }
-  });
+    });
 
 export default i18n;
--- project/src/sidebar/components/questionnaire_space.jsx
+++ project/src/sidebar/components/questionnaire_space.jsx
@@ -1,17 +1,30 @@
 import "../../App.css"
-import {useState} from "react";
+import { useEffect, useState } from "react";
 import "./questionnaire.css"
-import {useConfiguratorContext} from "../../contexts/MyContext.jsx";
+import { useConfiguratorContext } from "../../contexts/MyContext.jsx";
 import Obstruction from "./obstruction";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Collapse from 'react-bootstrap/Collapse';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import {FloatingLabel, ToggleButton} from "react-bootstrap";
+import { FloatingLabel, ToggleButton } from "react-bootstrap";
+import { useTranslation } from 'react-i18next'
+
 
 function Questionnaire_space() {
+    //i18n
+    const { t, i18n } = useTranslation();
+
+    useEffect(() => {
+        const lng = navigator.language;
+        i18n.changeLanguage(lng);
+    }, [])
+
+    const lng = navigator.language;
+
+
     //Uses reactcontext
-    const { dimensions, setDimensions} = useConfiguratorContext();
+    const { dimensions, setDimensions } = useConfiguratorContext();
     //Changes value of context
     const changeWidth = (event) => {
         setDimensions({ ...dimensions, width: event.target.value });
@@ -130,71 +143,73 @@ function Questionnaire_space() {
                 </tbody>
             </table>
         </div>*/
-            <Form className="overflow-auto">
-                <Form.Group>
-                    <Form.Label>Wat zijn de afmetingen van de woonruimte?</Form.Label>
-                    <div className="mb-3">
-                        <ButtonGroup>
-                            <ToggleButton
-                                onClick={() => setOpen(!open)}
-                                type="radio"
-                                value="Rectangular"
-                                variant="danger"
-                                checked={open}
-                            >
-                                Rechthoekig
-                            </ToggleButton>
-                            <ToggleButton
-                                type="radio"
-                                value="Other"
-                                variant="danger"
-                                checked={!open}
-                                onClick={() => setOpen(false)}>
-                                anders
-                            </ToggleButton>
-                        </ButtonGroup>
-                    </div>
-                    <Collapse in={open}>
-                        <div>
-                            <FloatingLabel
-                                controlId="floatingInput"
-                                label="Lengte"
-                                className="mb-4"
-                            >
-                                <Form.Control type="number" min={0} step={0.1} value={dimensions.length} size="sm"
-                                              onChange={changeLength} />
-                            </FloatingLabel>
-                            <FloatingLabel
-                                controlId="floatingInput"
-                                label="Breedte"
-                                className="mb-4"
-                            >
-                                <Form.Control type="number" min={0} step={0.1} value={dimensions.length} size="sm"
-                                              onChange={changeWidth} />
-                            </FloatingLabel>
-                            <FloatingLabel
-                                controlId="floatingInput"
-                                label="Hoogte"
-                                className="mb-4"
-                            >
-                                <Form.Control type="number" min={0} step={0.1} value={dimensions.length} size="sm"
-                                              onChange={changeHeight} />
-                            </FloatingLabel>
-                        </div>
-                    </Collapse>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Voeg toe met welke aspecten we in uw woonruimte rekening moeten houden.</Form.Label>
-                    <Button onClick={addObstr} variant="danger">Voeg aspect toe</Button>
+        <Form className="overflow-auto">
+            <Form.Group>
+                <Form.Label>Wat zijn de afmetingen van de woonruimte?</Form.Label>
+                <div className="mb-3">
+                    <ButtonGroup>
+                        <ToggleButton
+                            onClick={() => setOpen(!open)}
+                            type="radio"
+                            value="Rectangular"
+                            variant="danger"
+                            checked={open}
+                        >
+                            {t('questionnaire_space.rectangular')}
+                        </ToggleButton>
+                        <ToggleButton
+                            type="radio"
+                            value="Other"
+                            variant="danger"
+                            checked={!open}
+                            onClick={() => setOpen(false)}>
+                            {t('questionnaire_space.other')}
+                        </ToggleButton>
+                    </ButtonGroup>
+                </div>
+                <Collapse in={open}>
                     <div>
-                        {Obst.map((item) => (<Obstruction key={item}/>))}
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label={t('questionnaire_space.length')}
+                            className="mb-4"
+                        >
+                            <Form.Control type="number" min={0} step={0.1} value={dimensions.length} size="sm"
+                                onChange={changeLength} />
+                        </FloatingLabel>
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label={t('questionnaire_space.width')}
+                            className="mb-4"
+                        >
+                            <Form.Control type="number" min={0} step={0.1} value={dimensions.length} size="sm"
+                                onChange={changeWidth} />
+                        </FloatingLabel>
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label={t('questionnaire_space.height')}
+                            className="mb-4"
+                        >
+                            <Form.Control type="number" min={0} step={0.1} value={dimensions.length} size="sm"
+                                onChange={changeHeight} />
+                        </FloatingLabel>
                     </div>
-                </Form.Group>
+                </Collapse>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Voeg toe met welke aspecten we in uw woonruimte rekening moeten houden.</Form.Label>
+                <Button onClick={addObstr} variant="danger">
+                    {t('questionnaire_space.add_aspect')}
+                </Button>
+                <div>
+                    {Obst.map((item) => (<Obstruction key={item} />))}
+                </div>
+            </Form.Group>
 
-            </Form>
+        </Form>
 
 
-)
+    )
 }
 
 export default Questionnaire_space;
