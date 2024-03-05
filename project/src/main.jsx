5703a6e636ad7534bc399005c762193b47834327
Wiebe Vandendriessche <wiebe.vandendriessche@ugent.be>
2024-03-05 10:15:18 +0100

    i18n setup (en,nl,fr)

--- project/src/main.jsx
+++ project/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {ConfiguratorProvider, ContactProvider, VariaProvider} from "./contexts/MyContext";
+import './i18n.ts'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
