3246e2655f0a8693f1c0e92162472328d1c6f005
algansem <150685329+algansem@users.noreply.github.com>
2024-03-13 15:59:23 +0100

    added first part for opening door

--- project/src/sidebar/components_sidebar/obstruction.jsx
+++ project/src/sidebar/components_sidebar/obstruction.jsx
@@ -1,12 +1,12 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import "./questionnaire.css"
 import Form from "react-bootstrap/Form";
-import {Col, FloatingLabel, Row} from "react-bootstrap";
+import { Col, FloatingLabel, Row, ButtonGroup, ToggleButton } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 // eslint-disable-next-line react/prop-types
-function Obstruction({deleteObst, changeLength, changeHeight, changeWidth, changeType, type, obstId, length, width, height}) {
+function Obstruction({ deleteObst, changeLength, changeHeight, changeWidth, changeType, type, obstId, length, width, height }) {
     //i18n
     const { t, i18n } = useTranslation();
 
@@ -24,16 +24,36 @@ function Obstruction({deleteObst, changeLength, changeHeight, changeWidth, chang
         setShow1(!showButton1)
     };
 
-    console.log(obstId);
+    const [insideLeft, setInsideLeft] = useState(false);
+    const [insideRight, setInsideRight] = useState(false);
+    const [outside, setOutside] = useState(false);
+
+    const changeInsideLeft = () => {
+        setInsideLeft(true)
+        setInsideRight(false)
+        setOutside(false)
+    }
+    const changeInsideRight = () => {
+        setInsideLeft(false)
+        setInsideRight(true)
+        setOutside(false)
+    }
+    const changeOutside = () => {
+        setInsideLeft(false)
+        setInsideRight(false)
+        setOutside(true)
+    }
+
+    
     return (
         <div className="obstruction-bg m5">
-            <input type="button" id={"button"+obstId} value={type ?? t('obstructions.type')} onClick={showButton}  />
-            <Button className={"fa-rectangle-xmark"} variant={"danger"} id={"delete"+obstId} onClick={(e)=>deleteObst(e)}>
+            <input type="button" id={"button" + obstId} value={type ?? t('obstructions.type')} onClick={showButton} />
+            <Button className={"fa-rectangle-xmark"} variant={"danger"} id={"delete" + obstId} onClick={(e) => deleteObst(e)}>
                 x
             </Button>
             <div className="m5" hidden={showButton2}>
                 <Form.Group className="mb-3">
-                    <Form.Select name="type" id={"type"+obstId} onChange={(e)=>{changeType(e)}}>
+                    <Form.Select name="type" id={"type" + obstId} onChange={(e) => { changeType(e) }}>
                         <option value={t('obstructions.window')}>{t('obstructions.window')}</option>
                         <option value={t('obstructions.door')}>{t('obstructions.door')}</option>
                         <option value={t('obstructions.radiator')}>{t('obstructions.radiator')}</option>
@@ -48,34 +68,66 @@ function Obstruction({deleteObst, changeLength, changeHeight, changeWidth, chang
                         <Row>
                             <Col>
                                 <FloatingLabel
-                                    controlId={"length"+obstId}
+                                    controlId={"length" + obstId}
                                     label={t('questionnaire_space.length')}
                                     className="mb-4"
                                 >
-                                    <Form.Control type="number" name={"length"+obstId} min={0} step={0.1} defaultValue={length} onChange={(e)=>changeLength(e)}/>
+                                    <Form.Control type="number" name={"length" + obstId} min={0} step={0.1} defaultValue={length} onChange={(e) => changeLength(e)} />
                                 </FloatingLabel>
                             </Col>
                             <Col>
                                 <FloatingLabel
-                                    controlId={"width"+obstId}
+                                    controlId={"width" + obstId}
                                     label={t('questionnaire_space.width')}
                                     className="mb-4"
                                 >
-                                    <Form.Control type="number" name={"width"+obstId} min={0} step={0.1} defaultValue={width} onChange={(e)=>changeWidth(e)}/>
+                                    <Form.Control type="number" name={"width" + obstId} min={0} step={0.1} defaultValue={width} onChange={(e) => changeWidth(e)} />
 
                                 </FloatingLabel>
                             </Col>
                             <Col>
                                 <FloatingLabel
-                                    controlId={"height"+obstId}
+                                    controlId={"height" + obstId}
                                     label={t('questionnaire_space.height')}
                                     className="mb-4"
                                 >
-                                    <Form.Control type="number" name={"height"+obstId} min={0} step={0.1} defaultValue={height} onChange={(e)=>changeHeight(e)}/>
+                                    <Form.Control type="number" name={"height" + obstId} min={0} step={0.1} defaultValue={height} onChange={(e) => changeHeight(e)} />
                                 </FloatingLabel>
                             </Col>
                         </Row>
-
+                    </div>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Hoe opent de deur?</Form.Label>
+                    <div>
+                        <ButtonGroup>
+                            <ToggleButton
+                                onClick={changeInsideLeft}
+                                type="radio"
+                                value="Rectangular"
+                                variant="danger"
+                                checked={insideLeft}
+                            >
+                                Naar binnen, links in de ruimte
+                            </ToggleButton>
+                            <ToggleButton
+                                onClick={changeInsideRight}
+                                type="radio"
+                                value="Rectangular"
+                                variant="danger"
+                                checked={insideRight}
+                            >
+                                Naar binnen, rechts in de ruimte
+                            </ToggleButton>
+                            <ToggleButton
+                                onClick={changeOutside}
+                                type="radio"
+                                value="Other"
+                                variant="danger"
+                                checked={outside}>
+                                Naar buiten
+                            </ToggleButton>
+                        </ButtonGroup>
                     </div>
                 </Form.Group>
             </div>
