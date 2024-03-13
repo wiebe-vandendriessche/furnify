28d91a9bb1e6b2d56f7ef95c1b6fa80bfb40c04b
Xander Vanparys <xander.vanparys@ugent.be>
2024-03-13 15:34:29 +0100

    show tv wand default

--- project/src/3D/Scene.jsx
+++ project/src/3D/Scene.jsx
@@ -19,7 +19,7 @@ const Scene = () => {
         <Canvas className="canvas" camera={{position: [10, 6, 8]}} style={{ backgroundColor: 'lightblue' }}>
             <ambientLight intensity={.5} />
             <directionalLight position={[-10, 6, -8]} />
-            <Bed_assembly />
+            <Tv_wand />
             <Room width={width} depth={depth} height={height} wallThickness={0.3} floorThickness={0.3}/>
             <fog attach="fog" args={['lightblue', 1, 500]} />
             <Ground />
