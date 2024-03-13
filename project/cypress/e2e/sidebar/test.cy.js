456ce7685a18b323cac8b31112f548065e844425
Thomas Lonneville <thomas.lonneville@ugent.be>
2024-03-13 20:31:39 +0000

    Revert "trying to fix pipeline"
    This reverts commit 2d95e8878ec1ed066aeb7023923be421e47df46a

--- project/cypress/e2e/sidebar/test.cy.js
+++
@@ -1,7 +0,0 @@
-describe('First test',()=>{
-    it('Looking for logo', ()=>{
-        //Visits the baseUrl that is passed in cypress.config.js
-        cy.visit('')
-        cy.get('img#logo').should('be.visible')
-    })
-})
