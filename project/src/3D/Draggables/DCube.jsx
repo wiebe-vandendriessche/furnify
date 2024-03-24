62a3dfdf2403213456b65fac2314e0ac7d1f56a1
Wiebe Vandendriessche <wiebe.vandendriessche@ugent.be>
2024-03-24 15:41:54 +0100

    draggable in grid testing

---
+++ project/src/3D/Draggables/DCube.jsx
@@ -0,0 +1,24 @@
+import { MathUtils, Color } from 'three'
+import { useCallback, useRef, useEffect } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { easing } from 'maath'
+import { Grid, useDrag } from './Grid'
+
+export const DCube = ({ position = [0.5, 0.5, -0.5], c = new Color(), round = Math.round, clamp = MathUtils.clamp, ...props }) => {
+    const ref = useRef()
+    const pos = useRef(position)
+    const onDrag = useCallback(({ x, z }) => (pos.current = [round(clamp(x, -5, 4)) + 0.5, position[1], round(clamp(z, -5, 4)) + 0.5]), [])
+    const [events, active, hovered] = useDrag(onDrag)
+    useEffect(() => void (document.body.style.cursor = active ? 'grabbing' : hovered ? 'grab' : 'auto'), [active, hovered])
+    useFrame((state, delta) => {
+        easing.damp3(ref.current.position, pos.current, 0.1, delta)
+        easing.dampC(ref.current.material.color, active ? 'white' : hovered ? 'lightblue' : 'orange', 0.1, delta)
+    })
+    return (
+        <mesh ref={ref} castShadow receiveShadow {...events} {...props}>
+            <boxGeometry />
+            <meshStandardMaterial />
+        </mesh>
+    )
+};
--- project/src/3D/Scene.jsx
+++ project/src/3D/Scene.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Canvas, useThree, useFrame } from '@react-three/fiber';
-import { useDrag } from '@use-gesture/react';
 import { useState, useRef } from 'react'
 import { OrbitControls, Sky, Stars } from '@react-three/drei'
 import { Room } from './roomComponents/Room.tsx'
@@ -9,52 +8,8 @@ import { useConfiguratorContext } from '../contexts/ConfiguratorContext.jsx';
 import { Tv_wand } from './models/Tv_wand.jsx';
 import { Bed } from './models/Bed.jsx';
 import { Bed_assembly } from './models/Bed_assembly.jsx';
-// import { DraggableDodecahedron } from './Draggable.jsx';
-
-
-const DraggableDodecahedron = () => {
-    const colors = ['hotpink', 'red', 'blue', 'green', 'yellow'];
-    const ref = useRef();
-    const orbitControlsRef = useRef();
-    const [colorIdx, setColorIdx] = useState(0);
-    const [position, setPosition] = useState([0, 0, 0]);
-    const { size, viewport } = useThree();
-    const aspect = size.width / viewport.width;
-    useFrame(() => {
-        ref.current.rotation.z += 0.01;
-        ref.current.rotation.x += 0.01;
-    });
-    const bind = useDrag(
-        ({ down, offset: [x, y] }) => {
-            if (orbitControlsRef.current) {
-                orbitControlsRef.current.enabled = !down;
-            }
-            const [, , z] = position;
-            setPosition([x / aspect, -y / aspect, z]);
-        },
-        { pointerEvents: true }
-    );
-
-    return (
-        <>
-            <OrbitControls ref={orbitControlsRef} />
-            <mesh
-                position={position}
-                {...bind()}
-                ref={ref}
-                onClick={(e) => {
-                    setColorIdx((prevIdx) => (prevIdx === 4 ? 0 : prevIdx + 1));
-                }}
-                onPointerOver={(e) => console.log('hover')}
-                onPointerOut={(e) => console.log('unhover')}
-            >
-                <boxGeometry args={[1, 1, 1]} />
-                <meshLambertMaterial color={colors[colorIdx]} />
-            </mesh>
-        </>
-    );
-}
-
+import { DCube } from './Draggables/DCube.jsx';
+import { Grid, useDrag } from './Draggables/Grid.jsx'
 
 
 
@@ -67,13 +22,14 @@ const Scene = () => {
         <Canvas className="canvas" camera={{ position: [10, 6, 8] }} style={{ backgroundColor: 'lightblue' }}>
             <ambientLight intensity={.5} />
             <directionalLight position={[-10, 6, -8]} />
-            <DraggableDodecahedron />
-            <Room width={width} depth={depth} height={height} wallThickness={0.3} floorThickness={0.3} />
+            //<Room width={width} depth={depth} height={height} wallThickness={0.3} floorThickness={0.3} />
             <fog attach="fog" args={['lightblue', 1, 500]} />
             <Ground />
-
+            <Grid scale={10}>
+                <DCube position={[0.5, 1, -0.5]} scale={[1, 2, 1]} />
+            </Grid>
+            <OrbitControls makeDefault />
             <Stars radius={500} depth={50} count={8000} factor={15} saturation={50} fade speed={1} />
-
             <hemisphereLight color="lightblue" groundColor="0xf7e497" intensity={0.5} />
             <axesHelper />
         </Canvas>
