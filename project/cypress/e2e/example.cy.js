d9d6154d7f8c3a199cb35a758375f413acc20e98
Thomas Lonneville <thomas.lonneville@ugent.be>
2024-03-24 15:01:26 +0100

    saving work so I can checkout other branch

--- project/cypress/e2e/example.cy.js
+++ project/cypress/e2e/example.cy.js
@@ -1,7 +1,6 @@
 describe('First test',()=>{
-  it('Looking for logo', ()=>{
-    //Visits the baseUrl that is passed in cypress.config.cjs
 
+  beforeEach('visit site', ()=>{
     cy.visit('', {
       onBeforeLoad(win) {
         Object.defineProperty(win.navigator, 'language', { value: 'nl-BE' });
@@ -11,22 +10,40 @@ describe('First test',()=>{
       headers: {
         'Accept-Language': 'nl',
       },
-    });
-    cy.get('img#logo').should('be.visible')
+    });  })
+
+
+  it('Looking for logo', ()=>{
+    //Visits the baseUrl that is passed in cypress.config.cjs
+    cy.get('img#logo').should('be.visible');
   });
+
+
   it('Sidebar should be open', ()=>{
-    cy.visit('');
     cy.get('nav.nav-menu').should('be.visible');
   });
 
 
   it('Clicking sidebar should close it', ()=>{
-    cy.visit('');
     cy.get('div.menu-bars > svg').click();
     cy.get('nav.nav-menu').should('not.be.visible');
   });
 
+  it('Passing dimensions to rectangular form', ()=>{
+    let badInput="RANDOM_TEXT";
+    //cy.get('input.btn-check[value="Rectangular"]')
+    //TODO: add datatest-id/names see: https://docs.cypress.io/guides/references/best-practices
+    cy.get('.btn-group > :nth-child(2)').click();
+    cy.get('input#rectangularLength').clear().type(badInput); //passing unallowed value
+    cy.get('input#rectangularWidth').clear().type("7"); //clearing full input and passing a value
+    cy.get('input#rectangularHeight').clear().type("2");
+    cy.contains(badInput).should("not.exist");  //making sure input doesn't get saved
+  })
 
+  it("Making sure data doesn't get lost when going to next part of sidebar and back", ()=>{
+    cy.get('[datatest-id="btn-nav-sidebar-next"]').click();
+    cy.get('[datatest-id="btn-nav-sidebar-previous"]').click();
+  })
 
 
 
