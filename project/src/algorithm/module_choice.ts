f3a90c1213355c0dd0ce72727ad560c78e34f9b7
algansem <150685329+algansem@users.noreply.github.com>
2024-03-27 15:59:10 +0100

    read from test file

--- project/src/algorithm/module_choice.ts
+++ project/src/algorithm/module_choice.ts
@@ -1,10 +1,19 @@
+import { parseCsvData } from "./read_file_csv";
+
 const MIN_HEIGHT = 2.24;
 
 export const height_check = (height: number) => {
-    if(height < MIN_HEIGHT){
-        console.log("Kamer is niet hoog genoeg voor de module, minimuum hoogte: " + MIN_HEIGHT + " cm, jouw hoogte: "+ height +" cm")
-    }else{
+    if (height < MIN_HEIGHT) {
+        console.log("Kamer is niet hoog genoeg voor de module, minimuum hoogte: " + MIN_HEIGHT + " cm, jouw hoogte: " + height + " cm")
+    } else {
         console.log("kamer van " + height + "cm is hoog genoeg");
     }
-    
+
+    try {
+        const parsedData = parseCsvData();
+        console.log(parsedData);
+        console.log('test') // Do something with the parsed data
+    } catch (error) {
+        console.error('Error parsing CSV:', error);
+    }
 }
