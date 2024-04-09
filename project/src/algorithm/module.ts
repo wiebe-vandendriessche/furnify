02fd6f95eec125d00eb8a5faa1d2525d2bf00a47
algansem <150685329+algansem@users.noreply.github.com>
2024-04-09 14:42:41 +0200

    made class to save info

---
+++ project/src/algorithm/module.ts
@@ -0,0 +1,81 @@
+export class Module {
+    private _name: string;
+    private _height: number;
+    private _depth: number;
+    private _open: number;
+    private _closed: number;
+    private _saved: number;
+    private _bed: boolean = false;
+    private _sofa: boolean = false;
+    private _desk: boolean = false;
+    private _storage: boolean = false;
+    private _width_options: { key: number,value: number; }[] = [];
+
+
+
+    constructor(mod: any) {
+        this._name = mod.naam;
+        this._height = +mod.hoogte;
+        this._depth = +mod.diepte;
+        this._open = +mod.open;
+        this._closed = +mod.dicht;
+        this._saved = +mod.besparing;
+
+        if (mod.zetel == 'true') {
+            this._sofa = true
+        }
+
+        if (mod.opkladbed == 'true' || mod.bed_bewegend == 'true') {
+            this._bed = true
+        }
+
+        if (mod.kast_met_zijschappen == 'true' || mod.kast == 'true' ||
+            mod.kast_bewegend == 'true' || mod.tweede_kast_bewegend == 'true') {
+            this._storage = true;
+        }
+        if (mod.bureau == 'true' || mod.bureau_bewegend == 'true') {
+            this._desk = true;
+        }
+
+        this._width_options.push({key: 140, value: +mod.breedte140})
+        this._width_options.push({key: 160, value: +mod.breedte160})
+        this._width_options.push({key: 180, value: +mod.breedte180})
+    }
+
+    public get name(): string {
+        return this._name;
+    }
+
+
+    public get height(): number {
+        return this._height;
+    }
+
+
+    public get depth(): number {
+        return this._depth;
+    }
+
+
+    public get open(): number {
+        return this._open;
+    }
+
+
+    public get closed(): number {
+        return this._closed;
+    }
+
+
+    public get saved(): number {
+        return this._saved;
+    }
+
+    public width_options(size: number): number {
+        return this._width_options[size].value;
+    }
+
+    public type(bed: boolean, desk: boolean, sofa: boolean, storage: boolean){
+        return this._bed == bed && this._desk == desk && this._sofa == sofa && this._storage == storage
+    }
+}
--- project/src/algorithm/module_choice.ts
+++ project/src/algorithm/module_choice.ts
@@ -1,19 +1,21 @@
+import { Module } from "./module";
 import { parseCsvData } from "./read_file_csv";
 const MARGE = 1
-let modules: String[] | undefined = undefined;
+let modules: Module[]= []
 
-export const check = (height: number) => {
-    
-    if(modules == undefined){
+export const check = () => {
+
+    if(modules.length == 0){
         get_modules();
     }
+    console.log(modules)
 }
 
 const get_modules = () => {
     try {
         const parsedData = parseCsvData();
         console.log(parsedData);
-        parsedData.then(e => modules = e)
+        parsedData.then(e => e.forEach(ev => modules.push(new Module(ev))))
     } catch (error) {
         console.error('Error parsing CSV:', error);
     }
