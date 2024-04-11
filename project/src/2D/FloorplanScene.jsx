20beb0203894653e3beb058fefb693efa8b555d2
cpzdeef <xander.vanparys@gmail.com>
2024-04-11 15:39:54 +0200

    Add drawing controls to FloorplanScene

--- project/src/2D/FloorplanScene.jsx
+++ project/src/2D/FloorplanScene.jsx
@@ -7,21 +7,48 @@ import { use2d } from "../contexts/2dContext";
 
 export const FloorplanScene = () => {
   const { isDrawing, toggleDrawing, drawingCanvasRef } = use2d();
+
+  const handleDrawingButtonClick = (event) => {
+    event.stopPropagation();
+    toggleDrawing();
+  }
+
+
   return (
     <>
-      {/* <div> sui </div> */}
-      <Canvas
-        ref={drawingCanvasRef}
-        className="canvas"
-        orthographic
-        camera={{ position: [0, 0, 5], zoom: 100 }}
-      >
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <FloorplanEditor />
-        <axesHelper />
-        <OrbitControls enableZoom={true} enablePan={true} enableRotate={false} />
-      </Canvas>
+      <div ref={drawingCanvasRef} className="canvas">
+        <div
+          className="editor-controls"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            margin: "10px",
+            zIndex: 100,
+          }}
+        >
+          <button onClick={handleDrawingButtonClick}>
+            {isDrawing ? "Stop Drawing" : "Start Drawing"}
+          </button>
+        </div>
+
+        <Canvas
+          // ref={drawingCanvasRef}
+          className="canvas"
+          orthographic
+          camera={{ position: [0, 0, 5], zoom: 100 }}
+        >
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <FloorplanEditor />
+          <axesHelper />
+          <OrbitControls
+            enableZoom={true}
+            enablePan={true}
+            enableRotate={false}
+          />
+        </Canvas>
+      </div>
     </>
   );
 };
