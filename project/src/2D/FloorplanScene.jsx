4da48bec41899c37cb3bea2926db8e725daabd06
cpzdeef <xander.vanparys@gmail.com>
2024-04-12 23:05:21 +0200

    moved grid to seperate component

--- project/src/2D/FloorplanScene.jsx
+++ project/src/2D/FloorplanScene.jsx
@@ -12,6 +12,7 @@ import {
   Vector3,
 } from "three";
 import * as THREE from "three";
+import { GridComponent } from "./components/GridComponent";
 import "./Floorplan.css";
 
 export const FloorplanScene = () => {
@@ -57,54 +58,6 @@ export const FloorplanScene = () => {
   const handleGridButtonCLicked = (event) => {
     event.stopPropagation();
     setShowGrid((prev) => !prev);
-  };
-
-  const GridComponent = ({
-    size = 100,
-    divisions = 10,
-    color = "grey",
-    centerLineColor = "white",
-  }) => {
-    const material = new THREE.LineBasicMaterial({ color });
-    const centerLineMaterial = new THREE.LineBasicMaterial({
-      color: centerLineColor,
-    });
-
-    // Prepare points for grid lines
-    const points = [];
-    const step = size / divisions;
-    const halfSize = size / 2;
-
-    // Generate horizontal lines (parallel to X-axis)
-    for (let i = 0; i <= divisions; i++) {
-      points.push([
-        new THREE.Vector3(-halfSize, i * step - halfSize, 0),
-        new THREE.Vector3(halfSize, i * step - halfSize, 0),
-      ]);
-    }
-
-    // Generate vertical lines (parallel to Y-axis)
-    for (let i = 0; i <= divisions; i++) {
-      points.push([
-        new THREE.Vector3(i * step - halfSize, -halfSize, 0),
-        new THREE.Vector3(i * step - halfSize, halfSize, 0),
-      ]);
-    }
-
-    // JSX to render all lines
-    return (
-      <>
-        {points.map((line, index) => (
-          <Line
-            key={index}
-            points={line}
-            color={index % (divisions + 1) === 0 ? centerLineColor : color}
-            lineWidth={1} // Adjust line width as necessary
-            userData={{ test: "line" }} // Custom user data
-          />
-        ))}
-      </>
-    );
   };
 
   return (
