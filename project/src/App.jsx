310a046b0517e186393c4068b3ac9114deb7f269
Wiebe Vandendriessche <wiebe.vandendriessche@ugent.be>
2024-04-29 10:02:04 +0200

    added loading screen and removed strictmode for double rendering

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -3,24 +3,28 @@ import Sidebar from './sidebar/Sidebar'
 import Scene from './3D/Scene'
 import { useConfiguratorContext } from './contexts/ConfiguratorContext'
 import { FloorplanScene } from './2D/FloorplanScene'
-import { get_modules} from './algorithm/module_choice'
-
-
+import { get_modules } from './algorithm/module_choice'
+import { Loader } from '@react-three/drei'
+import { Suspense } from 'react'
 function App() {
 
   const { rectangular, setRectangular } = useConfiguratorContext();
   get_modules();
 
   return (
-    <div className="App">
-      <Sidebar />
-      <main>
-        <div className="container">
-          {rectangular && <Scene />}
-          {!rectangular && <FloorplanScene />}
+    <>
+      <Suspense fallback={null} >
+        <div className="App">
+          <Sidebar />
+          <main>
+            <div className="container">
+              {rectangular && <Scene />}
+              {!rectangular && <FloorplanScene />}
+            </div>
+          </main>
         </div>
-      </main>
-    </div>
+      </Suspense>
+    </>
   );
 }
 
